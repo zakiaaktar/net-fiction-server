edde2124c3deb73580f69fee8b870948e2ff3f40
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-10-28 14:12:57 +0200

    booking update part done

--- index.js
+++ index.js
@@ -72,7 +72,20 @@ async function run() {
     });
 
 
-    
+    app.patch('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
 
 
     app.delete('/bookings/:id' , async(req, res) => {
@@ -94,14 +107,11 @@ async function run() {
     //     const id = req.params.id;
     //     const query = { _id: new ObjectId(id) };
 
-
     //     const options = {
            
     //         // Include only the `title` and `imdb` fields in the returned document
     //         projection: { title: 1, price: 1, id: 1 },
     //       };
-
-
 
 
     //     const result = await serviceCollection.findOne(query, options);
@@ -110,9 +120,6 @@ async function run() {
 
 
    
-
-
-
 
 
 
